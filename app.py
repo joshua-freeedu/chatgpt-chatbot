b99303e0cd3cf546c0cb1320f8f4372b56fc685d
jpcabangon <cabangonjoshuap@gmail.com>
2023-02-27 11:14:11 +0800

    "enter key event listener minor bug"

--- app.py
+++ app.py
@@ -87,13 +87,19 @@ def main():
     # Add an event handler for the 'enter' key press
     if user_message and st.session_state.last_key_pressed == "enter":
         send_message()
-
     # Add an event listener for the 'enter' key press
     if "last_key_pressed" not in st.session_state:
         st.session_state.last_key_pressed = None
-
-    st.script("document.addEventListener('keydown', function(event) {if (event.code == 'Enter') {streamlit_component_on_change({'key': 'last_key_pressed', 'value': 'enter'});}});")
-
+    st.write("""
+        <script>
+            const input = document.getElementById('user_message');
+            input.addEventListener('keyup', function(event) {
+                if (event.code === 'Enter' && event.target.value.trim()) {
+                    streamlit_component_on_change({'key': 'last_key_pressed', 'value': 'enter'});
+                }
+            });
+        </script>
+    """)
     # Display the conversation history
     conversation = parse_conversation(st.session_state["conversation_history"])
     st.text_area("Chat", value=conversation, height=800, disabled=True)
